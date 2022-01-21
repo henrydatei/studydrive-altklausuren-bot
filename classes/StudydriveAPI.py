ec06134414532a2cd8e5b5933002f763ad5fe53d
henrydatei <31316025+henrydatei@users.noreply.github.com>
2022-01-21 16:20:04 +0100

    saveAllFilesOfACourse() fertig

--- classes/StudydriveAPI.py
+++ classes/StudydriveAPI.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from datetime import datetime
 
 class StudydriveAPI:
     token = None
@@ -18,7 +19,7 @@ class StudydriveAPI:
         req.raise_for_status()
         return req.json()['access_token']
 
-    def saveDocument(self, docID, filename): # from https://stackoverflow.com/questions/56950987/download-file-from-url-and-save-it-in-a-folder-python
+    def saveDocument(self, docID): # from https://stackoverflow.com/questions/56950987/download-file-from-url-and-save-it-in-a-folder-python
         # create folder for downloads
         if not os.path.exists("downloads"):
             os.makedirs("downloads")
@@ -26,12 +27,20 @@ class StudydriveAPI:
         req = requests.get('{}api/app/v1/documents/{}/download'.format(self.baseurl, docID), headers = headers, stream = True)
 
         if req.ok:
-            with open("downloads/" + filename, "wb") as f:
-                for chunk in req.iter_content(chunk_size = 1024 * 8):
-                    if chunk:
-                        f.write(chunk)
-                        f.flush()
-                        os.fsync(f.fileno())
+            docInfo = self.getDocumentInformation(docID)
+            folder = docInfo["file"]["course_name"]
+            filename = docInfo["file"]["file_name"]
+            if not os.path.exists("downloads/" + folder):
+                os.makedirs("downloads/" + folder)
+            if not os.path.exists("downloads/{}/{}-{}".format(folder, docID, filename)):
+                with open("downloads/{}/{}-{}".format(folder, docID, filename), "wb") as f:
+                    for chunk in req.iter_content(chunk_size = 1024 * 8):
+                        if chunk:
+                            f.write(chunk)
+                            f.flush()
+                            os.fsync(f.fileno())
+            else:
+                print("Document {}-{} already exists".format(docID, filename))
         else:  # HTTP status code 4XX/5XX
             print("Download failed: status code {}\n{}".format(req.status_code, req.text))
 
@@ -67,18 +76,26 @@ class StudydriveAPI:
         req = requests.post('{}api/app/v1/courses/{}/join'.format(self.baseurl, courseID), headers = headers)
         return req.json()
 
-    def joinAllCourses(self, universityID):
+    def getAllCourses(self, universityID):
         headers = {"authorization": "Bearer " + self.token}
         req = requests.get('{}/api/app/v1/universities/{}/courses'.format(self.baseurl, universityID), headers = headers)
-        courses = req.json()
-        for course in courses:
-            self.joinCourse(course["course_id"])
+        return req.json()
 
     def getMyCourses(self):
         headers = {"authorization": "Bearer " + self.token}
         req = requests.get('{}api/app/v1/users/left_sidebar'.format(self.baseurl), headers = headers)
         req.raise_for_status()
         return req.json()
+    
+    def joinAllCourses(self, universityID):
+        myCourseIDs = []
+        courseIDs = []
+        for myCourse in self.getMyCourses()["courses"]:
+            myCourseIDs.append(myCourse["id"])
+        for course in self.getAllCourses(universityID):
+            courseIDs.append(course["course_id"])
+        for courseID in set(courseIDs) - set(myCourseIDs):
+            self.joinCourse(courseID)
 
     def setCourseOrder(self, courses):
         headers = {"authorization": "Bearer " + self.token}
@@ -101,10 +118,17 @@ class StudydriveAPI:
         print(self.setCourseOrder(ids[::-1]))
 
     def getDocumentInformation(self, docID):
-        pass
+        headers = {"authorization": "Bearer " + self.token}
+        req = requests.get('{}api/app/v1/documents/{}/details'.format(self.baseurl, docID), headers = headers)
+        req.raise_for_status()
+        return req.json()
 
     def isDocumentUp(self, docID):
-        pass
+        try:
+            self.getDocumentInformation(docID)
+            return True
+        except:
+            return False
 
     def getMyCourses(self):
         headers = {"authorization": "Bearer " + self.token}
@@ -112,5 +136,28 @@ class StudydriveAPI:
         req.raise_for_status()
         return req.json()
 
-    def saveAllFilesOfACourse(self, courseID, type = 30): # 30 für Altklausur?
-        pass
+    # type 60 = Klausuren
+    # type 30 = Übungen & Tutorien
+    # type 80 = Zusammenfassungen
+    # type 10 = Andere
+    def saveAllFilesOfACourse(self, courseID, type = 60):
+        for file in self.getFileListofCourse(courseID):
+            if file["file_type"] == type:
+                self.saveDocument(file["file_id"])
+
+    def getFileListofCourse(self, courseID):
+        liste = []
+        page = 0
+        hasMore = True
+        headers = {"authorization": "Bearer " + self.token}
+        while hasMore:
+            parameters = {"sort": "time", "page": page, "semester_from": 0, "semester_until": 0, "type_ids": 0}
+            if page > 0:
+                parameters["reference_time"] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            req = requests.get('{}api/app/v1/feed/courses/{}/documents'.format(self.baseurl, courseID), headers = headers, params = parameters)
+            req.raise_for_status()
+            daten = req.json()
+            liste.extend(daten["files"])
+            hasMore = daten["has_more"]
+            page = page + 1
+        return liste
